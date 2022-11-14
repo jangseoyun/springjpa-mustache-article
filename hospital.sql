4b837c587a04c39046404629cc181d2a780d6d01
seoyun <may-primavera@naver.com>
2022-11-14 13:28:08 +0900

    commit: hospital 수원시 피부과 like 검색

--- hospital.sql
+++ hospital.sql
@@ -1,3 +1,4 @@
-select *
+select hospital_name
 from nation_wide_hospitals hospital
-where hospital.road_name_address like '%수원시%';
+where hospital.road_name_address like '%수원시%'
+and hospital.hospital_name like '%피부%';
