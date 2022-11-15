abf9e0dc0a208575f0db09faff1cf1a968154d40
seoyun <may-primavera@naver.com>
2022-11-15 14:02:48 +0900

    feat: restAPI service getHospital 구현

--- src/main/java/hospitaljpa/mustache/service/HospitalService.java
+++ src/main/java/hospitaljpa/mustache/service/HospitalService.java
@@ -1,13 +1,16 @@
 package hospitaljpa.mustache.service;
 
+import hospitaljpa.mustache.controller.HospitalResponse;
+import hospitaljpa.mustache.domain.dto.HospitalFactory;
 import hospitaljpa.mustache.domain.entity.Hospital;
 import hospitaljpa.mustache.domain.repository.HospitalJpaRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +21,11 @@ public class HospitalService {
 
     public Slice<Hospital> getHospitalList(Pageable pageable) {
         return hospitalJpaRepository.findAll(pageable);
+    }
+
+    public HospitalResponse getHospital(Long id) {
+        Optional<Hospital> findOne = hospitalJpaRepository.findById(id);
+        HospitalResponse hospitalResponse = HospitalFactory.toHospitalResponse(findOne.get());
+        return hospitalResponse;
     }
 }
