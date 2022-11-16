4b84bd285b3829d2590baeca95d9fb6a2f4ab3ab
seoyun <may-primavera@naver.com>
2022-11-16 14:03:59 +0900

    feat: article restAPI save controller, service

--- src/main/java/hospitaljpa/mustache/controller/ArticleRestController.java
+++ src/main/java/hospitaljpa/mustache/controller/ArticleRestController.java
@@ -1,14 +1,12 @@
 package hospitaljpa.mustache.controller;
 
+import hospitaljpa.mustache.domain.dto.ArticleRequest;
 import hospitaljpa.mustache.domain.dto.ArticleResponse;
 import hospitaljpa.mustache.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RestController
@@ -18,11 +16,21 @@ public class ArticleRestController {
 
     private final ArticleService articleService;
 
+    /*------------ select one ----------*/
     @GetMapping("/{id}")
     public ResponseEntity<ArticleResponse> getArticleOne(@PathVariable("id") Long id) {
         ArticleResponse articleOne = articleService.getArticleOne(id);
         return ResponseEntity
                 .ok()
                 .body(articleOne);
+    }
+
+    /*------------ save article ----------*/
+    @PostMapping("")
+    public ResponseEntity<ArticleResponse> save(ArticleRequest articleRequest) {
+        ArticleResponse articleResponse = articleService.saveArticle(articleRequest);
+        return ResponseEntity
+                .ok()
+                .body(articleResponse);
     }
 }
--- src/main/java/hospitaljpa/mustache/service/ArticleService.java
+++ src/main/java/hospitaljpa/mustache/service/ArticleService.java
@@ -1,16 +1,19 @@
 package hospitaljpa.mustache.service;
 
 import hospitaljpa.mustache.domain.dto.ArticleFactory;
+import hospitaljpa.mustache.domain.dto.ArticleRequest;
 import hospitaljpa.mustache.domain.dto.ArticleResponse;
 import hospitaljpa.mustache.domain.entity.Article;
 import hospitaljpa.mustache.domain.repository.ArticleJpaRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Slf4j
+@Transactional
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
@@ -18,9 +21,20 @@ public class ArticleService {
     private final ArticleJpaRepository articleJpaRepository;
 
     /*------- select one --------------*/
+    @Transactional(readOnly = true)
     public ArticleResponse getArticleOne(Long id) {
         Optional<Article> getArticle = articleJpaRepository.findById(id);
         log.info("service getArticle: {}", getArticle.get());
         return ArticleFactory.toArticleResponse(getArticle.get());
     }
+
+    /*------------ save article ----------*/
+    public ArticleResponse saveArticle(ArticleRequest articleRequest) {
+        Article article = ArticleFactory.toArticle(articleRequest);
+        Article save = articleJpaRepository.save(article);
+        log.info("저장: {}", save);
+        return ArticleFactory.toArticleResponse(save);
+    }
+
+
 }
