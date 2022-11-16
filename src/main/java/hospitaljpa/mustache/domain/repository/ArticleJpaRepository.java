56220cd374e883cb3fc8eedf6d8eb7bce6404769
seoyun <may-primavera@naver.com>
2022-11-16 13:09:41 +0900

    feat: ArticleJpaRepository 생성

--- src/main/java/hospitaljpa/mustache/domain/repository/ArticleJpaRepository.java
+++ src/main/java/hospitaljpa/mustache/domain/repository/ArticleJpaRepository.java
@@ -3,5 +3,5 @@ package hospitaljpa.mustache.domain.repository;
 import hospitaljpa.mustache.domain.entity.Article;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {
+public interface ArticleJpaRepository extends JpaRepository<Article, Long> {
 }
