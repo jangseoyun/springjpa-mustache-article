393cc32af87850580011cf04ebd576bb566bc8de
seoyun <may-primavera@naver.com>
2022-11-17 12:22:56 +0900

    feat: Users Factory 엔티티 변환, userJoinResponse 변환 (가입 결과 Message 생성)

---
+++ src/main/java/hospitaljpa/mustache/domain/factory/UsersFactory.java
@@ -0,0 +1,29 @@
+package hospitaljpa.mustache.domain.factory;
+
+import hospitaljpa.mustache.domain.dto.UserJoinRequest;
+import hospitaljpa.mustache.domain.dto.UserJoinResponse;
+import hospitaljpa.mustache.domain.entity.Users;
+
+public class UsersFactory {
+
+    public static Users toUsers(UserJoinRequest userJoinRequest) {
+        return new Users(
+                  userJoinRequest.getUsername()
+                , userJoinRequest.getUsername());
+    }
+
+    public static UserJoinResponse toUserJoinResponse(Users users) {
+        return new UserJoinResponse(
+                users.getUsername()
+                ,setMessage(users.getId())
+        );
+    }
+
+    private static String setMessage(Long id) {
+        if (id == null) {
+            return "이미 가입되어있는 아이디 입니다";
+        }
+
+        return "가입이 완료 되었습니다";
+    }
+}
