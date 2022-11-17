008fc439115992dce823d4a8b4d68ec23aaf9d60
seoyun <may-primavera@naver.com>
2022-11-17 14:24:29 +0900

    feat: 회원 아이디 존재 여부 체크 쿼리 (username으로 검색)

--- src/main/java/hospitaljpa/mustache/domain/repository/UserJpaRepository.java
+++ src/main/java/hospitaljpa/mustache/domain/repository/UserJpaRepository.java
@@ -2,6 +2,13 @@ package hospitaljpa.mustache.domain.repository;
 
 import hospitaljpa.mustache.domain.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserJpaRepository extends JpaRepository<Users, Long> {
+
+    @Query("select u from Users u where u.username =:username")
+    Optional<Users> findByUsername(@Param("username") String username);
 }
