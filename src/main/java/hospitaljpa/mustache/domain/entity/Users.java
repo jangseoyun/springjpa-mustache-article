27c02a77551caf5833c25c2caa951eee74eecfe6
seoyun <may-primavera@naver.com>
2022-11-17 12:19:31 +0900

    feat: Users Entity 생성 (username unique)

---
+++ src/main/java/hospitaljpa/mustache/domain/entity/Users.java
@@ -0,0 +1,31 @@
+package hospitaljpa.mustache.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Table(name = "t_users")
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @Column(name = "password")
+    private String password;
+
+    public Users(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
