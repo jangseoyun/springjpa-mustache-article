b115d308d4d7498eee8f5ccca3bc3ec708c5e98f
seoyun <may-primavera@naver.com>
2022-11-22 14:33:16 +0900

    feat: hospital review entity 생성 (연관관계 매핑)

---
+++ src/main/java/hospitaljpa/mustache/domain/entity/HospitalReview.java
@@ -0,0 +1,34 @@
+package hospitaljpa.mustache.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "t_hospital_review")
+public class HospitalReview {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "content")
+    private String review;
+
+    @Column(name = "patient_name")
+    private String patientName;
+
+}
