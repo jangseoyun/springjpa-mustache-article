7a8b02801f5e8299ab47b11b60a7ccfff193c7f0
seoyun <may-primavera@naver.com>
2022-11-24 11:40:45 +0900

    feat: hospital review factory toEntity

---
+++ src/main/java/hospitaljpa/mustache/domain/factory/ReviewFactory.java
@@ -0,0 +1,15 @@
+package hospitaljpa.mustache.domain.factory;
+
+import hospitaljpa.mustache.domain.dto.ReviewDto;
+import hospitaljpa.mustache.domain.entity.Hospital;
+import hospitaljpa.mustache.domain.entity.HospitalReview;
+
+public class ReviewFactory {
+    public static HospitalReview toReviewEntity(ReviewDto reviewDto, Hospital hospital) {
+        return new HospitalReview(
+                  hospital
+                , reviewDto.getTitle()
+                , reviewDto.getComment()
+                , reviewDto.getUserName());
+    }
+}
