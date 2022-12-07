84403e136e23992f7600146f5de6a92f3d37715f
seoyun <may-primavera@naver.com>
2022-12-07 12:49:41 +0900

    feat: visit entity

---
+++ src/main/java/hospitaljpa/mustache/domain/entity/Visit.java
@@ -0,0 +1,34 @@
+package hospitaljpa.mustache.domain.entity;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "t_visit")
+public class Visit {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "visit_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "hospital_id")
+    private Hospital hospital;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private Users users;
+
+    @Column(name = "disease")
+    private String disease;
+
+    @Column(name = "amount")
+    private int amount;
+
+}
