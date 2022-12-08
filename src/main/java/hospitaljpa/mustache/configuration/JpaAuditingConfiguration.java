7f7ad81c69a6d686d1fc7e035b974dca53e258f3
seoyun <may-primavera@naver.com>
2022-12-08 11:35:18 +0900

    feat: EnableJpaAuditing Auditing 기능 활성화

---
+++ src/main/java/hospitaljpa/mustache/configuration/JpaAuditingConfiguration.java
@@ -0,0 +1,9 @@
+package hospitaljpa.mustache.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfiguration {
+}
